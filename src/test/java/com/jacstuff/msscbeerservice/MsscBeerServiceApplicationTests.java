f061e7c97ca11d1a1366d7ad12c51096ae0c4797
johncrawley <johncrawley1@gmail.com>
2020-10-01 16:54:27 +0100

    Initial commit, changed pom xml to include jaxb in the properties

---
+++ src/test/java/com/jacstuff/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacstuff.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
