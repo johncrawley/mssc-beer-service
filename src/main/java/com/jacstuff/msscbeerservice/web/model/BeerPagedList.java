65e4371a69e56b05de5264ee52f33264d6ffcdea
johncrawley <johncrawley1@gmail.com>
2020-11-18 17:11:29 +0000

    Add JsonCreator example for paged list

--- src/main/java/com/jacstuff/msscbeerservice/web/model/BeerPagedList.java
+++ src/main/java/com/jacstuff/msscbeerservice/web/model/BeerPagedList.java
@@ -5,9 +5,24 @@ import java.util.List;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+
 public class BeerPagedList extends PageImpl<BeerDto>{
 
-	public BeerPagedList(List<BeerDto> content) {
+	@JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+	public BeerPagedList(@JsonProperty("content")List<BeerDto> content,
+						@JsonProperty("number") int number,
+						@JsonProperty("size") int size,
+						@JsonProperty("totalElements") Long totalElements,
+						@JsonProperty("pageable") JsonNode pageable,
+						@JsonProperty("last") boolean last,
+						@JsonProperty("totalPages") int totalPages,
+						@JsonProperty("sort") JsonNode sort,
+						@JsonProperty("fjrst") boolean first,
+						@JsonProperty("numberOfElements") int numberOfElements) {
+					
 		super(content);
 	}
 	
